f5660bd2eef1467c190d4051f57efb94f5d99b56
jpengsong@gmail.com <jpengsong@gmail.com>
2019-01-20 00:42:12 +0800

    123

--- app - 副本/store/main/Plist.js
+++
@@ -1,9 +0,0 @@
-Ext.define('App.store.main.Plist', {
-    extend: 'Ext.data.Store',
-    alias: 'store.main.Plist',
-    fields: ["xtype"],
-    data: [
-        { xtype: "login" },
-        { xtype: "home" }
-    ]
-});
